204b2cbd32ef4a3da72d38bdb9cb29e81fc9edbb
mcorreia <mascorreia@users.noreply.github.com>
2019-11-25 17:54:41 +0000

    Initial commit

---
+++ app/src/main/java/com/dam/kiddo/DefinitionsActivity.java
@@ -0,0 +1,14 @@
+package com.dam.kiddo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DefinitionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.definitions_activity);
+    }
+}
