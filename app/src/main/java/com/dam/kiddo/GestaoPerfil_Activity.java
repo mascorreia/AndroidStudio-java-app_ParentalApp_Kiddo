1d85ec74212be8ac2911854138d2333cbb0ce22a
shady10 <180221101@estudantes.ips.pt>
2019-11-27 16:41:38 +0000

    Começo de gestão de perfil

---
+++ app/src/main/java/com/dam/kiddo/GestaoPerfil_Activity.java
@@ -0,0 +1,14 @@
+package com.dam.kiddo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GestaoPerfil_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestao_perfil_);
+    }
+}
