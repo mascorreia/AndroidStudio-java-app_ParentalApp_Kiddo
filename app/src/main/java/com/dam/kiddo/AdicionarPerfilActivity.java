207dc470ac148f5c91fee7970791c65b2986d85d
shadyy10 <1800221101@estudantes.ips.pt>
2019-12-11 18:12:04 +0000

    AdcionarPerfilActivity

---
+++ app/src/main/java/com/dam/kiddo/AdicionarPerfilActivity.java
@@ -0,0 +1,15 @@
+package com.dam.kiddo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdicionarPerfilActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_perfil);
+    }
+
+
+}
--- app/src/main/java/com/dam/kiddo/GestaoPerfil_Activity.java
+++ app/src/main/java/com/dam/kiddo/GestaoPerfil_Activity.java
@@ -109,7 +109,7 @@ public class GestaoPerfil_Activity extends AppCompatActivity {
                 mDisplayDate.setText(date);
             }
         };
-        
+
 
     }
 
