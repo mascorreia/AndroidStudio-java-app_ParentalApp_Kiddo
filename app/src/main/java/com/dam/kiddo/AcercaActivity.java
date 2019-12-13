32c1bf27e02de28bfa008c4e1d0db4c8de583e5e
shadyy10 <1800221101@estudantes.ips.pt>
2019-12-13 14:05:26 +0000

    Acerca activity

--- app/src/main/java/com/dam/kiddo/AcercaActivity.java
+++ app/src/main/java/com/dam/kiddo/AcercaActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class AcercaActivity extends AppCompatActivity {
 
@@ -12,6 +13,26 @@ public class AcercaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca);
+
+        TextView mTitleWindow = (TextView) findViewById(R.id.messageWindow);
+        TextView mMessageWindow = (TextView) findViewById(R.id.titleWindow);
+
+        mTitleWindow.setText("Kiddo Application\n");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String someMessage = "Esta aplicação foi realizada no âmbito da unicade curricular de Computação Móvel.\n" +
+                "Equipa de desenvolvimento:\n" +
+                "João Madeira\n" +
+                "Gonçalo Alexandre\n" +
+                "Miguel Correia\n" +
+                "Rafael Bandeira\n" +
+                "Miguel Carromeu\n";
+
+
+        for (int i=0; i < 1; i++){
+            stringBuilder.append(someMessage);
+        }
+        mMessageWindow.setText(stringBuilder.toString());
     }
 
     public void voltarDefinicoes(View view){
